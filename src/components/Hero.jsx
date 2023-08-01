151632e65710ca8fe6c224fae08f2a526bba6e3c
deividcuello <deividcuello@gmail.com>
2023-07-31 22:47:55 -0400

    Deployed website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { TypeAnimation } from 'react-type-animation';
 
 function Hero() {
   return (
-    <div className='container mx-auto min-h-screen pt-28 md:py-10 flex flex-col-reverse md:flex-row items-center justify-between text-primary px-2'>
+    <div className='container mx-auto min-h-screen pt-28 md:py-10 flex flex-col-reverse md:flex-row items-center justify-center gap-24 md:gap-0 md:justify-between text-primary px-2'>
         <div className='w-full' id='#'>
             <h2 className='text-7xl font-bold'>I'm Deivid Cuello</h2>
             <TypeAnimation
@@ -19,8 +19,8 @@ function Hero() {
                 repeat={Infinity}
               />
         </div>
-        <div className='flex justify-end'>
-            <img src="https://cdn.pixabay.com/photo/2015/09/09/21/12/monitor-933392_1280.jpg" alt="Hero-Img" className='w-4/5 ring ring-offset-8 ring-offset-black ring-primary -rotate-12 mr-10'/>
+        <div className='flex justify-center md:justify-end'>
+            <img src="https://cdn.pixabay.com/photo/2015/09/09/21/12/monitor-933392_1280.jpg" alt="Hero-Img" className='w-4/5 ring ring-offset-8 ring-offset-black ring-primary -rotate-12 md:mr-10'/>
         </div>
     </div>
   )
